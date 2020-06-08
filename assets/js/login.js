20db223ae042c5e6650111edd5191dd2ce5687c5
ifer-itcast <yangkangkang@itcast.cn>
2020-06-08 10:40:59 +0800

    用户注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -12,6 +12,7 @@ $(function () {
 
     // 自定义表单校验规则
     const form = layui.form;
+    const layer = layui.layer;
     form.verify({
         pwd: [
             /^[\S]{6,12}$/,
@@ -24,5 +25,24 @@ $(function () {
                 return '两次密码不一致';
             }
         }
+    });
+
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        // 1. 阻止默认行为
+        e.preventDefault();
+        // 2. 发起 AJAX 请求
+        const data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        };
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+            // 3. 响应成功后处理
+            if (res.status !== 0) {
+                return layer.msg(res.message);
+            }
+            layer.msg('注册成功，请登录！');
+            $('#link_login').click();
+        });
     });
 });

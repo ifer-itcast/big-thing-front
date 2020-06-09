1ca16d3cb3a5ca000a7fc9eea5f75685bda546c9
ifer-itcast <yangkangkang@itcast.cn>
2020-06-09 21:36:10 +0800

    新增文章分类

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,4 +1,5 @@
 $(function () {
+    const layer = layui.layer;
     // 获取文章分类列表
     function initArtCateList() {
         $.ajax({
@@ -11,4 +12,32 @@ $(function () {
     }
 
     initArtCateList();
+
+    let indexAdd = null;
+    // 添加文章分类
+    $('#btnAddCate').on('click', function () {
+        indexAdd = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '添加文章分类',
+            content: $('#dialog-add').html() // 注意操作
+        });
+    });
+
+    // 监听提交
+    $('body').on('submit', '#form-add', function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('新增分类失败！');
+                }
+                layer.msg('新增分类成功！');
+                layer.close(indexAdd); // 根据索引关闭对应的弹出层
+            }
+        });
+    });
 });

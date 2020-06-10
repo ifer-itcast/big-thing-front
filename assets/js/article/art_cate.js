f024c9285d8b9e010d53c9f95f23ab74af74f915
ifer-itcast <yangkangkang@itcast.cn>
2020-06-10 09:35:29 +0800

    更新文章分类数据

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,5 +1,6 @@
 $(function () {
     const layer = layui.layer;
+    const form = layui.form;
     // 获取文章分类列表
     function initArtCateList() {
         $.ajax({
@@ -37,6 +38,43 @@ $(function () {
                 }
                 layer.msg('新增分类成功！');
                 layer.close(indexAdd); // 根据索引关闭对应的弹出层
+            }
+        });
+    });
+
+    let indexEdit = null;
+    // 通过代理的方式为 btn-edit 绑定点击事件
+    $('tbody').on('click', '.btn-edit', function () {
+        indexEdit = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '修改文章分类',
+            content: $('#dialog-edit').html() // 注意操作
+        });
+
+        const id = $(this).attr('data-id');
+        // 发起请求获取对应分类的数据
+        $.ajax({
+            url: '/my/article/cates/' + id,
+            success: function (res) {
+                form.val('form-edit', res.data);
+            }
+        });
+    });
+
+    $('body').on('submit', '#form-edit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/updatecate',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新分类数据失败！');
+                }
+                layer.msg('更新分类数据成功！');
+                layer.close(indexEdit);
+                initArtCateList();
             }
         });
     });
